20d28d402e38101ae82172b3870d933c2eea831b
jj <jjestyy@gmail.com>
2020-11-29 22:22:07 +0700

    tests setup

--- src/test/java/com/github/jjestyy/testwork/task/TestData.java
+++ src/test/java/com/github/jjestyy/testwork/task/TestData.java
@@ -1,25 +1,61 @@
 package com.github.jjestyy.testwork.task;
 
-import org.json.simple.parser.JSONParser;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class TestData {
-    JSONParser jsonParser = new JSONParser();
+    private final static String specPath = "src/test/java/com/github/jjestyy/testwork/task/examples/spec.json";
+    private final static String oldPath = "src/test/java/com/github/jjestyy/testwork/task/examples/old.json";
+    private final static String newPath = "src/test/java/com/github/jjestyy/testwork/task/examples/new.json";
+    private final static String res1path = "src/test/java/com/github/jjestyy/testwork/task/examples/result1.json";
+    private final static String res2path = "src/test/java/com/github/jjestyy/testwork/task/examples/result2.json";
 
-    public static String getSpec() {
-        return null;
+    public static String getSpec(){
+        try {
+            return Files.readString(Paths.get(specPath));
+        } catch (IOException e) {
+            throw new RuntimeException("missed spec.json in examples");
+        }
     }
 
     public static String getOldJSON() {
-        return null;
+        try {
+            return Files.readString(Paths.get(oldPath));
+        } catch (IOException e) {
+            throw new RuntimeException("missed old.json in examples");
+        }
     }
 
     public static String getNewJSON() {
-        return null;
+        try {
+            return Files.readString(Paths.get(newPath));
+        } catch (IOException e) {
+            throw new RuntimeException("missed new.json in examples");
+        }
+    }
+
+    public static String getRes1JSON() {
+        try {
+            return Files.readString(Paths.get(res1path));
+        } catch (IOException e) {
+            throw new RuntimeException("missed result1.json in examples");
+        }
+    }
+
+    public static String getRes2JSON() {
+        try {
+            return Files.readString(Paths.get(res2path));
+        } catch (IOException e) {
+            throw new RuntimeException("missed result2.json in examples");
+        }
     }
 
     public static List<String> getMarks() {
-        return null;
+        return List.of("0000 0000 0000 0000 0000 0000 0000 0000",
+                "1000 0000 0000 0000 0000 0000 0000 0000",
+                "0100 0000 0000 0000 0000 0000 0000 0000",
+                "1111 1111 1111 1111 1111 1111 1111 1111");
     }
 }
--- src/test/java/com/github/jjestyy/testwork/task/service/GkshbezopsoxrValidationServiceTest.java
+++ src/test/java/com/github/jjestyy/testwork/task/service/GkshbezopsoxrValidationServiceTest.java
@@ -2,15 +2,20 @@ package com.github.jjestyy.testwork.task.service;
 
 import com.github.jjestyy.testwork.task.TestData;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class GkshbezopsoxrValidationServiceTest {
+    
     @Autowired
-    GkshbezopsoxrValidationService service;
+    private GkshbezopsoxrValidationService service;
 
     @Test
     void getValidatedGkshbezopsoxrJSON() {
