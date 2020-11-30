62c7cb643a06a35531173ff0b149217c7d8dedc5
jj <jjestyy@gmail.com>
2020-11-30 21:24:59 +0700

    tests

--- src/main/java/com/github/jjestyy/testwork/task/service/GkshbezopsoxrValidationService.java
+++ src/main/java/com/github/jjestyy/testwork/task/service/GkshbezopsoxrValidationService.java
@@ -1,10 +1,13 @@
 package com.github.jjestyy.testwork.task.service;
 
 import lombok.RequiredArgsConstructor;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Iterator;
 import java.util.Map;
 
 @Service
@@ -24,12 +27,10 @@ public class GkshbezopsoxrValidationService {
             JSONObject oldData = (JSONObject) oldJson.get(dataId);
             JSONObject resultData = (JSONObject) newJson.get(dataId);
             resultJson.remove(dataId);
+
             for (Map.Entry<String, String> set : rules.entrySet()) {
-                if(resultData.has(set.getKey())) {
-                    if (!checkRules(set.getValue(), mark)) {
-                        resultData.remove(set.getKey());
-                        resultData.put(set.getKey(), oldData.get(set.getKey()));
-                    }
+                if (!checkRules(set.getValue(), mark)) {
+                    replaceJsonObject(set.getKey(), resultData, findJsonObject(set.getKey(), oldData));
                 }
             }
             resultJson.put(dataId, resultData);
@@ -37,6 +38,54 @@ public class GkshbezopsoxrValidationService {
         } catch (JSONException e) {
             throw new RuntimeException("Bad forms format, JSON parsing error");
         }
+    }
+
+    private void replaceJsonObject(String index, JSONObject data, JSONObject objectToPut) throws JSONException {
+        if(objectToPut != null) {
+            Iterator<String> keys = data.keys();
+            while (keys.hasNext()) {
+                String key = keys.next();
+                if (key.equals(index)) {
+                    keys.remove();
+                    data.remove(key);
+                    data.put(key, objectToPut);
+                    return;
+                }
+                if (data.get(key) instanceof JSONObject) {
+                    replaceJsonObject(index, (JSONObject) data.get(key), objectToPut);
+                }
+
+                if (data.get(key) instanceof JSONArray) {
+                    JSONArray array = (JSONArray) data.get(key);
+                    for(int i = 0; i < array.length(); i++) {
+                        replaceJsonObject(index, (JSONObject) array.get(i), objectToPut);
+                    }
+                }
+            }
+        }
+    }
+
+    private JSONObject findJsonObject(String index, JSONObject data) throws JSONException {
+        Iterator<String> keys = data.keys();
+        JSONObject finded = null;
+        while(keys.hasNext() && finded == null) {
+            String key = keys.next();
+            if(key.equals(index)) {
+                finded = (JSONObject) data.get(key);
+                break;
+            } else {
+                if (data.get(key) instanceof JSONObject) {
+                    finded = findJsonObject(index, (JSONObject) data.get(key));
+                }
+                if (data.get(key) instanceof JSONArray) {
+                    JSONArray array = (JSONArray) data.get(key);
+                    for (int i = 0; i < array.length(); i++) {
+                        finded = findJsonObject(index, (JSONObject) array.get(i));
+                    }
+                }
+            }
+        }
+        return finded;
     }
 
     private boolean checkRules(String value, String mark) {
--- src/test/java/com/github/jjestyy/testwork/task/service/GkshbezopsoxrValidationServiceTest.java
+++ src/test/java/com/github/jjestyy/testwork/task/service/GkshbezopsoxrValidationServiceTest.java
@@ -1,8 +1,11 @@
 package com.github.jjestyy.testwork.task.service;
 
 import com.github.jjestyy.testwork.task.TestData;
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -18,8 +21,22 @@ class GkshbezopsoxrValidationServiceTest {
     private GkshbezopsoxrValidationService service;
 
     @Test
-    void getValidatedGkshbezopsoxrJSON() {
+    void getValidatedGkshbezopsoxrJSON() throws JSONException {
         List<String> marks = TestData.getMarks();
-        service.GetValidatedGkshbezopsoxrJSON(TestData.getSpec(), TestData.getOldJSON(), TestData.getNewJSON(), marks.get(0));
+//       "0000 0000 0000 0000 0000 0000 0000 0000"
+        JSONAssert.assertEquals(service.GetValidatedGkshbezopsoxrJSON(TestData.getSpec(), TestData.getOldJSON(), TestData.getNewJSON(), marks.get(0)),
+                TestData.getOldJSON(), JSONCompareMode.LENIENT);
+//       "1000 0000 0000 0000 0000 0000 0000 0000"
+        JSONAssert.assertEquals(service.GetValidatedGkshbezopsoxrJSON(TestData.getSpec(), TestData.getOldJSON(), TestData.getNewJSON(), marks.get(1)),
+                TestData.getRes1JSON(), JSONCompareMode.LENIENT);
+        //       "1111 1111 1111 1111 1111 1111 1111 1111"
+        JSONAssert.assertEquals(service.GetValidatedGkshbezopsoxrJSON(TestData.getSpec(), TestData.getOldJSON(), TestData.getNewJSON(), marks.get(0)),
+                TestData.getNewJSON(), JSONCompareMode.LENIENT);
+//       "0100 0000 0000 0000 0000 0000 0000 0000"
+        JSONAssert.assertEquals(service.GetValidatedGkshbezopsoxrJSON(TestData.getSpec(), TestData.getOldJSON(), TestData.getNewJSON(), marks.get(0)),
+                TestData.getRes2JSON(), JSONCompareMode.LENIENT);
+
+
+
     }
 }
