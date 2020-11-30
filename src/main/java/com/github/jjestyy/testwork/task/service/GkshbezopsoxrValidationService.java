c57ac2d6bddce101775a17e3d94360e98edaa4ab
jj <jjestyy@gmail.com>
2020-11-30 18:59:24 +0700

    filter func

--- src/main/java/com/github/jjestyy/testwork/task/service/GkshbezopsoxrValidationService.java
+++ src/main/java/com/github/jjestyy/testwork/task/service/GkshbezopsoxrValidationService.java
@@ -1,6 +1,8 @@
 package com.github.jjestyy.testwork.task.service;
 
 import lombok.RequiredArgsConstructor;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Map;
@@ -14,11 +16,37 @@ public class GkshbezopsoxrValidationService {
 
     public String GetValidatedGkshbezopsoxrJSON(String spec, String oldForm, String newForm, String mark)  {
         Map<String,String> rules = specRulesService.getRulesList(spec);
-
-        System.out.println(rules);
-        return null;
+        String dataId = "data";
+        try {
+            JSONObject oldJson = new JSONObject(oldForm);
+            JSONObject newJson = new JSONObject(newForm);
+            JSONObject resultJson = new JSONObject(newForm);
+            JSONObject oldData = (JSONObject) oldJson.get(dataId);
+            JSONObject resultData = (JSONObject) newJson.get(dataId);
+            resultJson.remove(dataId);
+            for (Map.Entry<String, String> set : rules.entrySet()) {
+                if(resultData.has(set.getKey())) {
+                    if (!checkRules(set.getValue(), mark)) {
+                        resultData.remove(set.getKey());
+                        resultData.put(set.getKey(), oldData.get(set.getKey()));
+                    }
+                }
+            }
+            resultJson.put(dataId, resultData);
+            return resultJson.toString();
+        } catch (JSONException e) {
+            throw new RuntimeException("Bad forms format, JSON parsing error");
+        }
     }
 
+    private boolean checkRules(String value, String mark) {
+        for(int i = 0; i< value.length(); i++) {
+            if(value.charAt(i) == '1' && mark.charAt(i) == '0') {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
 }
